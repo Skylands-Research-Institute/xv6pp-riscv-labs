110f42ed4727c6864887cd55a1170cd4e6743a90
John  Sissler <34193125+jsissler@users.noreply.github.com>
2026-05-15 17:26:14 -0400

    student starting code

---
+++ user/pingpong.c
@@ -0,0 +1,11 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  // TODO: use pipe(), fork(), read(), write(), and wait() to send one
+  // byte from parent to child and one byte back from child to parent.
+  exit(0);
+}
---
+++ user/sleep.c
@@ -0,0 +1,11 @@
+#include "kernel/types.h"
+#include "kernel/stat.h"
+#include "user/user.h"
+
+int
+main(int argc, char *argv[])
+{
+  // TODO: parse the sleep duration argument, call sleep(), and exit
+  // with a non-zero status if the command is used incorrectly.
+  exit(0);
+}
